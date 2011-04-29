72578d39b065410951527ccde15d4bb20e668f50
Jake McGinty <mcginty@illinois.edu>
2011-04-28 22:56:49 -0500

    Fixed to NOT switch to landscape mode on tilt, and added VideoRecordThread (still a skeleton).

---
+++ src/edu/uiuc/cs414/group8droid/VideoRecordThread.java
@@ -0,0 +1,18 @@
+package edu.uiuc.cs414.group8droid;
+
+import android.hardware.Camera;
+
+public class VideoRecordThread extends Thread {
+	SkeletonActivity parent;
+	Camera mCamera;
+	int cams;
+	
+	public VideoRecordThread(SkeletonActivity parent) {
+		this.parent = parent;
+	}
+	
+	public void run() {
+		mCamera = Camera.open();
+		mCamera.setPreviewDisplay(R.id.previewSurface);
+	}
+}
